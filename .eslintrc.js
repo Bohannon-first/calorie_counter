639eaf8fe561ca793f0fa7ec3c61c59798d29177
Ярослав <spartacus215@yandex.ru>
2021-11-08 20:49:25 +0300

    Создал конфигурационный файл

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2017': true,
+    'node': true,
+  },
+  'extends': 'htmlacademy/vanilla',
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
+  },
+  'rules': {
+  },
+};
